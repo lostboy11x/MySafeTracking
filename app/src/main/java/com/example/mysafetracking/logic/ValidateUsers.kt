77e93d2297a6ab13ce2039be4be6616c8082c9df
MarkGRX11 <marc11gandara@gmail.com>
2025-03-16 12:31:47 +0100

    Password change

--- app/src/main/java/com/example/mysafetracking/logic/ValidateUsers.kt
+++ app/src/main/java/com/example/mysafetracking/logic/ValidateUsers.kt
@@ -4,12 +4,13 @@ import android.util.Patterns
 
 // Funció per validar el login
 fun validateLogin(email: String, password: String): Boolean {
-    return email.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.isNotBlank()
+    return email.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(email).matches() &&
+            password.isNotBlank() && password.length >= 8
 }
 
 // Funció per validar el registre
 fun validateRegister(firstName: String, lastName: String, email: String, password: String): Boolean {
     return firstName.isNotBlank() && lastName.isNotBlank() &&
             email.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(email).matches() &&
-            password.isNotBlank()
+            password.isNotBlank() && password.length >= 8
 }
